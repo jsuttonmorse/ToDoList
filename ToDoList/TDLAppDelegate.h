bc97292e42b9c6007a62531d601724f8fef08aaf
Jonah Sutton-Morse <jsuttonmorse@gmail.com>
2014-02-22 15:34:05 -0600

    Initial Commit

---
+++ ToDoList/TDLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TDLAppDelegate.h
+//  ToDoList
+//
+//  Created by Jonah Sutton-Morse on 2/22/14.
+//  Copyright (c) 2014 Jonah Sutton-Morse. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TDLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ToDoList/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ToDoList
+//
+//  Created by Jonah Sutton-Morse on 2/22/14.
+//  Copyright (c) 2014 Jonah Sutton-Morse. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TDLAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TDLAppDelegate class]));
+    }
+}
